dd9b6a3a8e93f0cd7dfe4dc31fecd0219482675a
arreche <arreche@gmail.com>
2025-07-20 15:33:55 +0100

    remove redundant return_pct

--- metadata.py
+++ metadata.py
@@ -21,7 +21,6 @@ df['prev_close'] = df.groupby('symbol')['close'].shift(1)
 df['change_pct'] = ((df['close'] - df['prev_close']) / df['prev_close'] * 100).round(2)
 df['gap_pct'] = ((df['open'] - df['prev_close']) / df['prev_close'] * 100).round(2)
 df['run_pct'] = ((df['close'] - df['open']) / df['open'] * 100).round(2)
-df['return_pct'] = ((df['close'] - df['prev_close']) / df['prev_close'] * 100).round(2)
 df['avg_volume_20d'] = df.groupby('symbol')['volume'].transform(lambda x: x.rolling(window=20, min_periods=1).mean())
 df['relative_volume'] = (df['volume'] / df['avg_volume_20d']).round(2)
 
